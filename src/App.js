4966af5fc48b465af3e739702e69df833a882bb2
Joaquin Nayen <nayenjoaquin@gmail.com>
2022-05-14 03:51:01 -0400

    logo click

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import NewForm from './components/new_form/new-form';
 import Form from './components/form/form';
-import { HashRouter, Route, Routes} from 'react-router-dom';
+import {Route, Routes, useNavigate} from 'react-router-dom';
 import SubmitedForm from './components/new_form/submited-form';
 import SubmitedAnswer from './components/form/submited-answer';
 import logo from './LOGO.png';
 
 function App() {
+
+  const navigate = useNavigate();
   const logoClick = () => {
-    console.log("click en logo")
+    navigate('/',{replace:true})
   }
   return (
     <div className="App">
-      <HashRouter>
         <div className='page-navbar'><div className='logo-container'><img src={logo} className='logo-img' onClick={logoClick}/></div></div>
         <div className='background-pattern'></div>
         <div className='page-content'>
@@ -20,10 +21,8 @@ function App() {
           <Route path= '/form/:id' element={<Form/>}/>
           <Route path='/submited-form/:formId' element={<SubmitedForm/>}/>
           <Route path='/submited-answer' element={<SubmitedAnswer/>}/>
-          
         </Routes>
         </div>
-      </HashRouter>
     </div>
   );
 }
