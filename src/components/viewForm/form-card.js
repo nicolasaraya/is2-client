d9d34d1ed5db41e18ab95154df8d2ad100733871
Nicolas Araya <naraya2018@inf.udec.cl>
2022-06-05 12:42:15 -0400

    update

---
+++ src/components/viewForm/form-card.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const FormCard = (props) => {
+
+    const {pregunta, index, setIndex, length, setRespuestas, respuestas ,setLoading} = {...props};
+
+    useEffect(()=>{
+        const alternativas = document.getElementsByClassName("alter-pregunta-"+pregunta.id);
+        changeAlter(index,alternativas)
+    },[index])
+
+    const navigate = useNavigate();
+    const toggleCheck = (e,id) =>{
+        const alternativas = document.getElementsByClassName("alter-pregunta-"+pregunta.id);
+        const estaAlternativa = document.getElementById("alter-checkbox-"+id)
+        for(var i=0;i<alternativas.length;i++){
+            if(alternativas[i]!==estaAlternativa && alternativas[i].checked===true){
+                 alternativas[i].checked=false;
+            }
+        }
+        var auxAlter
+        auxAlter = respuestas
+        if(!estaAlternativa.checked) auxAlter[index-1] = null
+        else auxAlter[index-1] = id
+        console.log(auxAlter)
+        setRespuestas(auxAlter)
+    }
+    
+    const changeAlter = (index,alternativas) =>{
+
+        if(respuestas[index-1]>=0){
+            for(i=0;i<alternativas.length;i++){
+                if(alternativas[i].id=="alter-checkbox-"+respuestas[index-1]){
+                    alternativas[i].checked=true;
+                }
+                else alternativas[i].checked=false;
+            } 
+        }else{
+            for(var i=0;i<alternativas.length;i++){
+                alternativas[i].checked=false;
+            } 
+        }
+    }
+
+    const handleBack = () => {
+        const aux = index - 1;
+        setIndex(aux);
+    }
+
+    const handleNext = () => {
+        const aux = index +1;
+        setIndex(aux);
+    }
+
+    const handleSubmit = async() => {
+        var banderita = 0;
+        for(var  i = 0 ; i < length ; i++){
+            if(respuestas[i] == null ) banderita = 1;
+        }
+        if(banderita){
+            alert('No ha respondido todas las preguntas')
+        }
+        else {
+            setLoading(true);
+            const data = JSON.stringify(respuestas);
+            const res = await fetch('https://server-encuestas.herokuapp.com/newRespuesta',{
+                'method' : 'POST',
+                 headers : {
+                     'Content-Type':'application/json'
+                 },
+                 body:data
+             });
+            
+            if(res.status!=500) navigate(`/submited-answer`, {replace: true});
+            else{
+                await setLoading(false)
+                alert('Error interno de servidor: intente más tarde')
+            }
+        }
+
+    }
+    return(
+        <div className="form-card__container">
+            <p className="form-card__title">{index}) {pregunta.title}</p>
+            <div className="form-card__alter-container">
+                {
+                    pregunta.alter.map((val,index)=>{
+                        return(
+                            <div className="form-card__alter" key={val.id}>
+                                <input type='checkbox' className={"form-card__alter-checkbox alter-pregunta-"+pregunta.id} onChange={(e)=>toggleCheck(e,val.id[0])} id={'alter-checkbox-'+val.id}></input>
+                                <p className="form-card__alter-title">{val.title}</p>    
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            {
+                length===1
+                ? <div className="form-card__btn-container">
+                </div>
+                : index===1
+                ? <div className="form-card__btn-container">
+                    <button className="form-card__next-btn-only" onClick={handleNext}>Next{" >>"}</button>
+                </div>
+                : index===length
+                    ? <div className="form-card__btn-container">
+                        <button className="form-card__back-btn" onClick={handleBack}>{"<< "}Back</button>
+                    </div>
+                    : <div className="form-card__btn-container">
+                        <button className="form-card__back-btn" onClick={handleBack}>{"<< "}Back</button>
+                        <button className="form-card__next-btn" onClick={handleNext}>Next{" >>"}</button>
+                    </div>
+                
+            }
+        </div>
+    )
+}
+
+export default FormCard;
---
+++ src/components/viewForm/grafico.js
@@ -0,0 +1,96 @@
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import React from 'react'
+import Loading from "../loading";
+import { useEffect, useState } from "react";
+
+const Grafico = (props) => {
+
+    const {index, grafico, data} =  {...props}
+    const [loading, setLoading]  = useState(true);
+    const [graficos,setGraficos] = useState([[{}]]);
+    
+    
+
+    useEffect(() => {
+              let info = Object.entries(data)
+              //console.log(info[1][1])
+              //console.log(info[1][1][index])
+              //console.log(info[1][1][index].alter)
+              for(let i = 0; i < info[1][1].length; i++){
+                let size = info[1][1][i].alter.length
+                let grafico = [{}]
+                for(let j = 0; j < size; j++){
+                    let n = info[1][1][i].alter[j].title
+                    let num = info[1][1][i].alter[j].answers
+                    grafico.push({name: n, value: num})
+                    //console.log("nombre " + n+ " cant: " + num)
+                  }
+                graficos.push(grafico)
+              }
+              setGraficos(graficos)
+        
+              setLoading(false);
+              
+    },[]);
+
+
+    const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF8042"];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({
+        cx,
+        cy,
+        midAngle,
+        innerRadius,
+        outerRadius,
+        percent,
+        indexx
+      }: any) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+        return (
+          <text
+            x={x}
+            y={y}
+            fill="white"
+            textAnchor={x > cx ? "start" : "end"}
+            dominantBaseline="central"
+          >
+            {`${(percent * 100).toFixed(0)}%`}
+          </text>
+        );
+      };
+      console.log(grafico[index])
+      return (
+        <>
+        {
+            loading===false?
+                <PieChart width={400} height={400}>
+                  <Pie
+                    data={graficos[index]}
+                    cx={200}
+                    cy={200}
+                    labelLine={false}
+                    label={renderCustomizedLabel}
+                    outerRadius={150}
+                    fill="#8884d8"
+                    dataKey="value"
+                    >
+                    
+                    {grafico.map((entry, indexx) => (
+                      <Cell key={`cell-${indexx}`} fill={COLORS[indexx % COLORS.length]} />
+                    ))}
+                  </Pie>
+                </PieChart>
+                :<Loading></Loading>
+
+        }
+        
+        </>
+
+
+    )
+}
+
+export default Grafico;
